a1f9a315220be6002dc0ca82db3b8a27ca740bbc
Ruben Fiszel <ruben@rubenfiszel.com>
2025-03-02 21:44:13 +0100

    all

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -90,6 +90,7 @@ const DEFAULT_PORT: u16 = 8000;
 const DEFAULT_SERVER_BIND_ADDR: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
 
 mod ee;
+mod main_common;
 mod monitor;
 
 #[inline(always)]
@@ -115,27 +116,7 @@ where
 }
 
 pub fn main() -> anyhow::Result<()> {
-    // https://github.com/denoland/deno/blob/main/cli/main.rs#L477
-    #[cfg(feature = "deno_core")]
-    let unrecognized_v8_flags = deno_core::v8_set_flags(vec![
-        "--stack-size=1024".to_string(),
-        // TODO(bartlomieju): I think this can be removed as it's handled by `deno_core`
-        // and its settings.
-        // deno_ast removes TypeScript `assert` keywords, so this flag only affects JavaScript
-        // TODO(petamoriken): Need to check TypeScript `assert` keywords in deno_ast
-        "--no-harmony-import-assertions".to_string(),
-    ])
-    .into_iter()
-    .skip(1)
-    .collect::<Vec<_>>();
-
-    #[cfg(feature = "deno_core")]
-    if !unrecognized_v8_flags.is_empty() {
-        println!("Unrecognized V8 flags: {:?}", unrecognized_v8_flags);
-    }
-
-    #[cfg(feature = "deno_core")]
-    deno_core::JsRuntime::init_platform(None, false);
+    main_common::setup_deno_runtime()?;
     create_and_run_current_thread_inner(windmill_main())
 }
 
